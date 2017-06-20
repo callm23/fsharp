3343ef1370dd5390aa33fc639dbda577e89c62ca
allmannsperger.ch <allmannsperger.christian@fronius.com>
2017-06-20 07:35:22 +0200

    Add Functions part1

---
+++ src/LearningFSharp/LearningFSharp/Func.fs
@@ -0,0 +1,17 @@
+﻿module Func
+
+// Functions are values
+let add x y = x + y
+
+// as lambda
+let add' = fun x y -> x + y
+
+//let checkThis item f = //without annotation
+let checkThis (item: 'c) (f: 'c -> bool) : unit = //item is a generic
+  if f item then
+    printfn "HIT"
+  else
+    printfn "MISS"
+
+checkThis 5 (fun x -> x > 3)
+checkThis "str" (fun x -> x.Length > 5)
