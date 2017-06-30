b11b4aee3d8e5745b59dea61ab98808e60580c38
allmannsperger.ch <allmannsperger.christian@fronius.com>
2017-06-30 11:59:03 +0200

    learn options an tuples

---
+++ src/LearningFSharp/LearningFSharp/Options.fs
@@ -0,0 +1,21 @@
+﻿module Options
+
+// is a disciminated union
+// some(x)
+// None
+
+let o1 = Some(5)
+let o2 = None
+
+if o1 = o2 then
+  printfn "Values are equal"
+
+o1.IsSome
+
+let checkOption o =
+  match o with
+  | Some(x) -> printfn "Option contains value %A" x
+  | None -> printfn "Option doesn't have a value"
+
+checkOption o1
+checkOption o2
---
+++ src/LearningFSharp/LearningFSharp/Tuples.fs
@@ -0,0 +1,29 @@
+﻿module Tuples
+
+let t1 = 12, 5, 7
+let v1, v2, v3 = t1
+let v4, v5, _ = t1 // ignore 3rd value
+
+let getTuple =
+  (1,2)
+
+let a,b = getTuple
+
+let t2 = "hi", true
+// use built-in functions to get values from tuple (fst, snd)
+printfn "%A" (fst t2)
+printfn "%A" (snd t2)
+
+// or define your own function (third)
+let third t =
+  let _, _, r = t
+  r
+
+printfn "%A" (third t1)
+
+let third' (_,_,r) = r
+
+printfn "%A" (third' t1)
+
+
+
